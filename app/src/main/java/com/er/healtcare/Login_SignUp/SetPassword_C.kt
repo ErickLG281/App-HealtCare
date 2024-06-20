01186f61e462a04bfeb1ef41a8431da8398d32e3
ErickLG <132631485+ErickLG281@users.noreply.github.com>
2024-06-19 22:10:27 -0500

    Interfaz login b

--- app/src/main/java/com/er/healtcare/Login_SignUp/SetPassword_C.kt
+++ app/src/main/java/com/er/healtcare/Login_SignUp/SetPassword_C.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.er.healtcare.R
 
-class SetPassword_D : AppCompatActivity() {
+class SetPassword_C : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_set_password_d)
+        setContentView(R.layout.activity_set_password_c)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
